03ad0df6723fd253932d45e2639bb06f5d6566d9
sebastian.vega1 <sebastian.vega@acct.global>
2020-06-15 23:25:12 -0300

    sw added

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,7 @@ const path = require("path");
 const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
   entry: "./src/client/index.js",
@@ -29,5 +30,6 @@ module.exports = {
       filename: "./index.html",
     }),
     new MiniCssExtractPlugin({ filename: "[name].css" }),
+    new WorkboxPlugin.GenerateSW()
   ],
 };
