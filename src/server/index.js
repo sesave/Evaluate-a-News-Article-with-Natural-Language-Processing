d9df49d2019696a9549784e90493fb54271c042e
sebastian.vega1 <sebastian.vega@acct.global>
2020-06-15 23:07:37 -0300

    index server done

--- src/server/index.js
+++ src/server/index.js
@@ -11,32 +11,34 @@ app.use(express.static("dist"));
 
 console.log(__dirname);
 
-var textapi = new aylien({
-  application_id: server.env.API_ID,
-  application_key: server.env.API_KEY,
+var apiCredentials = new aylien({
+  application_id: process.env.API_ID,
+  application_key: process.env.API_KEY,
 });
 
 app.get("/", function (req, res) {
-  // res.sendFile('dist/index.html')
-  res.sendFile(path.resolve("src/client/views/index.html"));
+  res.sendFile("dist/index.html");
+  //res.sendFile(path.resolve("src/client/views/index.html"));
 });
 
-app.post("/api", (req, res) => {
-  const { text } = req.body;
-  console.log("Request to '/api' endpoint", text);
-  textapi.sentiment({ text }, (error, result, remaining) => {
-    console.log("Aylien Callback", result, remaining);
-    res.send(result);
-  });
-});
-
-app.post("/article", (req, res) => {
-  const { text } = req.body;
-  console.log("Request to '/article' endpoint", text);
-  textapi.sentiment({ url: text }, (error, result, remaining) => {
-    console.log("Aylien Callback", result, remaining);
-    res.send(result);
-  });
+app.post("/textApi", (req, res) => {
+  try {
+    var data = apiCredentials.sentiment(
+      {
+        text: req.body.text,
+      },
+      function (error, response) {
+        if (error === null) {
+          console.log(response);
+          res.send(response);
+        }
+      }
+    );
+    //res.send(mockAPIResponse)
+  } catch (error) {
+    // Passes errors into the handler
+    return next(error);
+  }
 });
 
 // designates what port the app will listen to for incoming requests
@@ -47,3 +49,5 @@ app.listen(8080, function () {
 app.get("/test", function (req, res) {
   res.send(mockAPIResponse);
 });
+
+module.exports = app;
